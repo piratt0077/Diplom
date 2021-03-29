f30cf2c9c7755c1c145c9624f70ee9a0861c3e28
piratt0077 <piratt0077@yandex.ru>
2021-03-29 14:37:33 +0500

    genious desicion+boring shit(formating reports)

--- routes/api.js
+++ routes/api.js
@@ -18,32 +18,34 @@ router.get('/search', async function(req, res) {
   M_Number.findOne({number:req.query.number})
   .then(async doc=>{
     ////place for test
+
     //function for sleep
     if(req.query.number==1){
       console.log("timeToSleep 1sec");
-      
       await new Promise(resolve => setTimeout(resolve, 2000));  
-      
     }
     if(req.query.number==5){
       console.log("timeToSleep 5sec");
       await new Promise(resolve => setTimeout(resolve, 7000));
     }
     ////////////////////////
+
     if (doc==undefined) {
       defnumber.number=req.query.number;
       return res.send({success:true,errorCode:0,data:defnumber});
     }
+    
 
 
 
     outdoc=doc.toJSON();
     delete outdoc.firstReportDate;
-    //nado li
     delete outdoc.lastReportDate;
     outdoc.id=outdoc._id;
     delete outdoc._id;
     outdoc.isBan=0;
+    var newreps=await analog(doc.reports);
+    outdoc.reports=newreps;
     if (outdoc.rating<9) {
       outdoc.isBan=1;
     }
@@ -55,5 +57,39 @@ router.get('/search', async function(req, res) {
     res.status(500).send({success:false,errorCode:1,data:err});
   })
   });
+
+///////скучный говнокод
+  function analog(reports){
+  
+    newreps=[];
+    reports.forEach(element => {
+      var boool=true;
+      newreps.forEach(elem=>{
+        if (element.type==elem.type) {
+          elem.count+=1;
+          boool=false;
+        }
+      })
+      if (boool) {
+        newreps.push({type:element.type,count:1})  
+      }
+    });
+    return newreps;
+  }
+///////гениальное дизайнерское решение
+function analog2(reports){
+  newreps=[];
+  reports.reduce(function(accum,curval){
+    if (newreps[curval.type]==undefined) {
+      newreps.type=[curval.type];
+
+    }
+    else {
+      newreps[curval.type]+=1;
+    }
+    return accum;
+  },newreps)
+  return newreps;
+}
 
 module.exports = router;
